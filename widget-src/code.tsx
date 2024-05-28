ca14ed6269b91caedf01252265c198550df6c292
MrMichelr <me@mr-michel.com>
2024-05-28 08:09:18 +0200

    Setup / constants, utils, modes

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -2,17 +2,59 @@
 // Simplist is a simple To Do List for Figma and Figjam
 
 const { widget } = figma
-const { AutoLayout } = widget
+const { useSyncedState } = widget
+
+// Imports
+import { Colors } from './constants'
+import { shadeColor } from './utils'
+
+// Components
 
 
 /* Main function that handle the whole Widget */
 function Main () {
 
-  return (
-    <></>
-  )
+  // State Machine
+  const [isHideCompleted, setHideCompleted] = useSyncedState('isHideCompleted', false)
+  const [mode, setMode] = useSyncedState('mode', {
+    powerMode: false,
+    editOpen: false,
+    settingOpen: false,
+    infoOpen: false,
+    firstOpen: false
+  })
+
+  // Colors
+  const [lightMode, setLightMode] = useSyncedState('lightMode', true)
+  const [color, setColor] = useSyncedState('color', {})
+
+  // Open Onboarding View
+  if (!mode.firstOpen) {
+    return (
+      <></>
+    )
+  }
+  // Open Setting View
+  if (!mode.settingOpen) {
+    return (
+      <></>
+    )
+  }
+  // Open Edit View
+  if (!mode.editOpen) {
+    return (
+      <></>
+    )
+  }
+  // Open PowerMode View
+  if (!mode.powerMode) {
+    return (
+      <></>
+    )
+  }
+
+  
  
 }
 
 widget.register(Main)
-
---
+++ widget-src/constants.tsx
@@ -0,0 +1,52 @@
+export const App = {
+    author: 'Michel R.',
+    version: '2.0',
+    name: 'SimpList',
+    dev: true,
+}
+
+export const Colors = {
+    neutrals: {
+        0   : '#FFFFFF',
+        100 : '#EFEFEF',
+        200 : '#DEDEDE',
+        300 : '#CCCCCC',
+        400 : '#B2B2B2',
+        500 : '#8A8A8A',
+        600 : '#6E6E6E',
+        700 : '#585858',
+        800 : '#333333',
+        900 : '#161616',
+        1000: '#000000',
+    },
+    red: {
+        100: '#FFB0B2',
+        200: '#F9868D',
+        300: '#EF5868',
+        400: '#EA3B56',
+        500: '#E30044',
+        600: '#C5003A',
+        700: '#A80030',
+        800: '#71001D',
+        900: '#3F000C',
+    },
+    accent: '#0038FF',
+}
+
+
+export const CornerRadius = {
+    s: 4,
+    m: 8,
+    l: 12,
+}
+
+export const Spacing = {
+    none: 0,
+    xxs : 4,
+    xs  : 8,
+    s   : 12,
+    m   : 16,
+    l   : 24,
+    xl  : 28,
+    xxl : 32,
+}
