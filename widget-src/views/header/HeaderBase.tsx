2b9320a0d6b88111e9844066b9e2377dc2617163
MrMichelr <me@mr-michel.com>
2024-05-29 21:53:59 +0200

    Refacto/ Icon Library + Empty Illu

--- widget-src/views/header/HeaderBase.tsx
+++ widget-src/views/header/HeaderBase.tsx
@@ -1,10 +1,13 @@
-import { Font, Spacing } from "../../constants"
-
 const { widget } = figma
 const {AutoLayout, Text, Line} = widget
 
+// Imports
+import {ColorPalette, Font, Spacing } from "../../constants"
+import { Icon } from "../Icon"
+
 type Props = {
     title: string
+    colors: ColorPalette
 }
 
 export function Header (props: Props) {
@@ -22,9 +25,10 @@ export function Header (props: Props) {
                 verticalAlignItems='center'
                 width='fill-parent'
             >
+                <Icon svg='logo' name='Logo' fill={props.colors.accent.medium} size={'large'}/>
                 <Text 
                     name='Title'
-                    fill='#161616'
+                    fill={props.colors.text.primary}
                     width='fill-parent'
                     fontFamily={Font.family}
                     fontSize={Font.heading.bolder.size}
@@ -33,7 +37,7 @@ export function Header (props: Props) {
                     {props.title}
                 </Text>
             </AutoLayout>
-            <Line name='Separator' stroke='#EFEFEF' length='fill-parent' />
+            <Line name='Separator' stroke={props.colors.neutrals.lowest} length='fill-parent' />
         </AutoLayout>
     )
 }
