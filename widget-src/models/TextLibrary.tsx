11e77fd4b8b13b06935a7729b46b26aa0dc62bdb
MrMichelr <me@mr-michel.com>
2024-06-12 23:31:12 +0200

    Refactor/ Input

--- widget-src/models/TextLibrary.tsx
+++ widget-src/models/TextLibrary.tsx
@@ -4,10 +4,11 @@ export const TextLibrary = {
         description: 'Start by typing something to add a new task!',
     },
     inputPlaceholder: 'Add a new task',
+    inputError: 'Error: Enter text to Add a Task',
     button: {
         info: 'Done',
         powerCancel: 'Cancel',
         powerSave: 'Save',
-    }
+    },
 
 }
